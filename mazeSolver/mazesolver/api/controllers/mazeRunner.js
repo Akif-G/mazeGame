e693d26e34638708aab222cce5448d613335f105
Akif Gultekin <mehmetgultekin@sabanciuniv.edu>
2020-07-23 03:40:14 +0300

    maze is now workin will add canvas

--- mazeSolver/mazesolver/api/controllers/mazeRunner.js
+++ mazeSolver/mazesolver/api/controllers/mazeRunner.js
@@ -4,31 +4,31 @@ const fs = require('fs');
 const AppError = require('../utils/AppError');
 const path = require('path');
 
-//maze solver is compiled c++ program, code is in mazeSolverCodes.
+//maze solver is compiled c++ program, code is in mazeSolverCodes; placed with server.js (beginning
 exports.solve = catchAsync(
     async (req, res, next) => {
         const { data } = req.body;
 
-        await fs.writeFile(`${path.join(__dirname, 'maze')}`, data, () => { })
+        await fs.writeFile(`${path.join(process.cwd(), 'maze')}`, data, () => { })
         //Arguements will take place in ,<>, here
-        await exec(`${path.join(__dirname, 'mazeSolver.exe')}`, (error, stdout, stderr) => {
+        exec(`${path.join(process.cwd(), 'mazeSolver.exe')}`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`);
-                return;
             }
             console.log(`stdout: ${stdout}`);
             console.error(`stderr: ${stderr}`);
+
+            fs.readFile(`${path.join(process.cwd(), 'results')}`, { encoding: 'utf8', flag: 'r' }, function (err, data) {
+                if (err) {
+                    console.log(err);
+                    const error = new AppError('Could not solve the maze', 500);
+                    next(error);
+                }
+                else {
+                    res.status(200).json({ data: data.split("\r\n").join(" ") });
+                }
+            });
         })
 
-        await fs.readFile(`${path.join(__dirname, 'results')}`, { encoding: 'utf8', flag: 'r' }, function (err, data) {
-            if (err) {
-                console.log(err);
-                const error = new AppError('Could not solve the maze', 500);
-                next(error);
-            }
-            else {
-                res.status(200).json({ data: data.split("\r\n").join(" ") });
-            }
-        });
     }
 )
